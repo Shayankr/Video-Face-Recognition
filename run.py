499719bee4d42579782ec2d77213e5e0d9be5fe1
Shayankr <22612941+Shayankr@users.noreply.github.com>
2023-09-10 00:20:30 +0530

    code to run detection.py script

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
-from app import create_app
+# from app import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from src.components.face_detection.face_detection import FaceDetection
+from config.configuration import DetectionConfig
+import os
+
+
+
+
+
+
+
+# Create an instance of the BBox Detection class
+face_detection_instance = FaceDetection()
+
+face_detection_instance.process_frames_continuously(DetectionConfig.frame_folder, DetectionConfig.bbox_json_path )
+print("BBox Detection Successfully Executed!")
+
